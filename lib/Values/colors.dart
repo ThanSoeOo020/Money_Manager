6b7a27e82383eeed3994385b216547c43de1cd7a
GF63 Thin 9RCX <thansoeoo@ucstgi.edu.mm>
2020-11-08 15:56:28 +0630

    framed

--- lib/Values/colors.dart
+++ lib/Values/colors.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-const primaryColor = HexColor("");
+class Style{
+
+  static Color primaryColor = HexColor("#46CB1B");
+  static Color secondaryColor = Colors.white;
+
+  static Color textColor = Colors.black;
+  static Color incomeColor = HexColor("#4CS886");
+  static Color expenseColor = HexColor("#46CB1B");
+}
